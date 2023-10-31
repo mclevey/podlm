4bac33c6d5661a4d8c943eef11ed5cafc8546e33
John McLevey <john.mclevey@uwaterloo.ca>
2023-10-31 18:04:03 -0400

    finished entities

---
+++ pipeline/label_entities/src/label_entities.py
@@ -0,0 +1,22 @@
+from podlm.utilities import load_configs, configure_logging, load_parquet, save_parquet
+from podlm.text import transformer_entities
+from span_marker import SpanMarkerModel
+import logging
+
+# print('🔥🔥🔥 conda activate transformers 🔥🔥🔥')
+
+model = SpanMarkerModel.from_pretrained("lxyuan/span-marker-bert-base-multilingual-uncased-multinerd")
+model.cuda()
+
+configure_logging(__file__)
+config, _ = load_configs()
+subreddits = config['subreddits']
+entity_score_threshold = config['entity_score_threshold']
+
+
+for r in subreddits:
+    df = load_parquet(f'{r}_sentences')
+    entities, entities_info = transformer_entities(df, model, entity_score_threshold, 'sentence', 'id_sentence')
+    save_parquet(entities, f'{r}_entities')
+    save_parquet(entities_info, f'{r}_entities_info')
+    logging.debug("Constructed entity dataframes (long and counts)") 
