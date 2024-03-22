4ee174accba11ac54bf10d7df4f01b890a76c8a6
tcrick <43351367+tcrick@users.noreply.github.com>
2024-03-22 12:42:00 -0400

    adding new query functions
    es_results_to_df: faster dataframe creation from ES results iterator
    es_run_query: execute provided query and return results iterator
    es_construct_query: helper function to craft complex queries, specific to reddit data

--- podlm/podlm/reddit.py
+++ podlm/podlm/reddit.py
@@ -3,7 +3,7 @@ import urllib3
 urllib3.disable_warnings()
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import scan
-from podlm.utilities import merge_string_columns
+from podlm.utilities import merge_string_columns, to_POSIX, from_POSIX
 
 def es_query_reddit(search: str, es: Elasticsearch):
     results = {}    
@@ -26,11 +26,17 @@ def es_query_reddit(search: str, es: Elasticsearch):
             df['id'] = 't1_' + df['id']
     return results['subs'], results['coms']
 
-
+''' OLD
 def es_results_to_df(search_results):
     df = pd.DataFrame.from_records(list(search_results))
     df = pd.concat([df, df['_source'].apply(pd.Series)], axis=1)
     return df
+'''
+# NEW (roughly 3x faster)
+def es_results_to_df(search_results):
+    df = pd.json_normalize(data = search_results)
+    df.columns = [col.split(".")[-1] for col in df.columns]
+    df['created_utc'] = df['created_utc'].astype(int)
 
 
 def set_dtypes(df: pd.DataFrame):
@@ -63,3 +69,99 @@ def merge_submissions_and_comments(submissions: pd.DataFrame, comments: pd.DataF
     comments.rename(columns={'body': 'text'}, inplace=True)
     df = pd.concat([submissions, comments])
     return df
+
+
+### NEW QUERY FUNCTIONS
+
+
+def es_run_query_reddit(query: dict, es: Elasticsearch, search_index: str = 'reddit-index-s'):
+    all_resp = scan(es, query = query, index= search_index)    
+    return all_resp
+    
+
+def es_construct_query(exact_terms = None,
+                           search_terms_all = None,
+                           search_terms_any = None,
+                           search_phrases_all = None,
+                           search_phrases_any = None,
+                           date_start = None,
+                           date_end = None):
+
+    '''
+    First five options expect a list of 2 item lists. The 2 item lists are field-string pairs. 
+    
+    Examples:
+    
+    exact_terms_to_search = [['subreddit', 'politics']]
+    search_terms_all = [['title', 'MAGA'],['domain','breitbart.com']]
+
+    
+    
+    date_start and date_end expect a list of integers [YYYY, MM, DD]
+    
+    Example to search first day of January, 2017:
+    
+    date_start = [2017,01,01]
+    date_end = [2017,01,02]
+    
+    '''
+
+    
+    search_query = {'query': {'bool': {}}}
+    date_range = {'range': {'created_utc': {'format':'epoch_second'}}}
+    
+    filter_list = []    
+    must_match_list = []    
+    should_match_list = []
+    
+    
+    if date_start:
+        start = to_POSIX(date_start)
+        date_range['range']['created_utc'].update({'gte':start})
+    
+    if date_end:
+        end = to_POSIX(date_end)
+        date_range['range']['created_utc'].update({'lt':end})
+        
+    if date_start or date_end:
+        filter_list.append(date_range)        
+    
+    if exact_terms:
+        for term in exact_terms:
+            term_dict = {'term': {term[0]: {'value': term[1], 'case_insensitive': True}}}
+            filter_list.append(term_dict)            
+            
+    if date_start or date_end or exact_terms:
+        search_query['query']['bool'].update({'filter':filter_list})
+        
+        
+    if search_terms_all:
+        for term in search_terms_all:
+            term_dict = {'match': {term[0]: {'query': term[1], 'fuzziness': 'AUTO'}}}
+            must_match_list.append(term_dict)
+            
+    if search_phrases_all:
+        for term in search_phrases_all:
+            term_dict = {'match_phrase': {term[0]: term[1]}}
+            must_match_list.append(term_dict)
+            
+    if search_terms_all or search_phrases_all:
+        search_query['query']['bool'].update({'must': must_match_list})
+        
+
+    if search_terms_any:
+        for term in search_terms_any:
+            term_dict = {'match': {term[0]: {'query': term[1], 'fuzziness': 'AUTO'}}}
+            should_match_list.append(term_dict)   
+            
+    if search_phrases_any:
+        for term in search_phrases_any:
+            term_dict = {'match_phrase': {term[0]: term[1]}}
+            should_match_list.append(term_dict) 
+            
+    if search_terms_any or search_phrases_any:
+        search_query['query']['bool'].update({'should': should_match_list})
+        search_query['query']['bool'].update({'minimum_should_match': 1})
+        
+    
+    return search_query
